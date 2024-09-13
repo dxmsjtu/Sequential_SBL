6010f1d71b2361d279b8f74ac73e3c315933967e
YONGSUNG PARK <52879020+ysparkwin@users.noreply.github.com>
2024-09-13 16:57:25 -0700

    Add files via upload

---
+++ SBLseq_v3p12.m
@@ -0,0 +1,289 @@
+function [ gamma, report] = SBLseq_v3p12( A , Y, gammaIn, options )
+%
+% function [ gamma , report ] = SBL_v3p1( A , Y, options )
+% The idea behind SBL is to find a diagonal replica 'covariance' Gamma.
+% Minimizing (YY^T / AGA^T + penality) should lead to the correct
+% replica selection (up to a bogus scale factor/amplitude).
+%
+% Attention: If Y is single snapshot (and single frequency), it needs to be
+% a row vector (the code makes a 2nd snapshot with repmat).
+%
+% Inputs
+%
+% A - Multiple frequency augmented dictionary <f , n , m>
+%     f: number of frequencies
+%     n: number of sensors
+%     m: number of replicas
+%   Note: if f==1, A = < n , m >
+%
+%
+% Y - Multiple snapshot multiple frequency observations <f , n , L>
+%     f: number of frequencies
+%     n: number of sensors
+%     L: number of snapshots
+%   Note: if f==1, Y = < n , L >
+%
+% options - see SBLset.m 
+%
+%
+% Outputs
+%
+% gamma <m , 1> - vector containing source power
+%                 1: surfaces found by minimum error norm
+%
+% report - various report options
+%
+%--------------------------------------------------------------------------
+% Version 1.0:
+% Code originally written by P. Gerstoft.
+%
+% Version 2.23
+% Edited to include multiple frequency support: 5/16/16
+%
+% Version 3.1
+% Different convergance norm and code update
+% A and Y have now one more dimensions
+% Posterior unbiased mean
+% Handles single snapshot
+%
+% Version 3.12
+% more efficient diagonal gamma computations
+%
+%
+% Kay L Gemba & Santosh Nannuru
+% MPL/SIO/UCSD gemba@ucsd.edu & snannuru@ucsd.edu
+
+%% check function
+
+if ismatrix(A) % SBL needs frequency dimension
+    B(1,:,:) = A;
+    A        = B;
+end
+
+% number of frequencies
+Nfreq     = size(A,1);
+
+% single frequency single snapshot
+if ismatrix(Y)
+    % either 1 freq or 1 snapshot
+    if  Nfreq == 1
+        
+        if size(Y,2) == 1 % single snapshot
+            Y=Y.'; %squeeze2
+        else
+            Y = permute(Y,[ 3 1 2 ]); % works
+        end
+        
+    end
+    
+end
+
+%%
+
+options.SBL_v = '3.12';
+
+%% slicing
+
+Nsource = options.Nsource;
+
+if options.tic == 1
+    tic
+end
+
+%% Initialize variables
+
+% number of sensors
+Nsensor   = size(A,2);
+% number of dictionary entries
+Ntheta    = size(A,3);
+% number of snapshots in the data covariance
+Nsnapshot = size(Y,3);
+% noise power initialization
+sigc      = ones(Nfreq,1) * options.noisepower.guess;
+% posterior
+x_post    = zeros(Nfreq, Ntheta, Nsnapshot);
+% minimum (global) gamma
+gmin_global   = realmax;
+% L1 error
+errornorm    = zeros(options.convergence.maxiter,1);
+
+% initialize equal and uncorrelated weights
+% gamma        = 1*ones(Ntheta,1);
+gamma_num    = zeros(Nfreq , Ntheta);
+gamma_denum  = zeros(Nfreq , Ntheta);
+
+% Sample Covariance Matrix
+SCM = zeros( Nfreq , Nsensor , Nsensor );
+
+for i_f = 1 : Nfreq
+    SCM(i_f,:,:) = squeeze2(Y(i_f,:,:)) * squeeze2(Y(i_f,:,:))' / Nsnapshot;
+end
+
+%% Initialize for Sequential processing change by YP
+% Simple source motion model (Random walk model with finite lags alphalag)
+alphalags = 2;  % cover [-alphalag , +alphalag] grid bin walk
+                % ex. Beamforming with 0.5[deg] angular grid bin with lag 2
+                % the previous estimate 0 deg -> informative to the next [-1:0.5:1] [deg]
+
+% alpha distribution
+alphalength = 2*alphalags+1; alphatmp = ones(1,alphalength);  %uniform
+% alphalength = 2*alphalags+1; alphatmp = rand(1,alphalength);  %random
+% alphatmp = normpdf(-alphalags:alphalags,0,1);                 %gaussian, here with variance 1
+
+alpha = alphatmp / sum(alphatmp);
+
+Ftmp = zeros(Ntheta,1); Ftmp(1:alphalags+1) = alpha(alphalags+1:2*alphalags+1);
+
+F = toeplitz(Ftmp);
+% F = eye(Ntheta);
+
+% gammaIn = conv(gammaIn, power(alpha,2)); gammaIn(1:alphalags)=[]; gammaIn(end-alphalags+1:end)=[];
+gammaIn = diag(F * diag(gammaIn) * F.');
+
+gamma        = 1*ones(Ntheta,1) * max(gammaIn)/1000;
+
+% Af = squeeze(A(:,:,iF));
+rho = 1.0;
+
+gamma = rho*rho*gammaIn + gamma;
+
+%% Main Loop
+
+display(['Sequential SBL version ', options.SBL_v ,' initialized.']);
+
+for j1 = 1 : options.convergence.maxiter
+    
+    % for error analysis
+    gammaOld = gamma;
+    
+    %% gamma update
+    
+    for i_f = 1 : Nfreq
+        
+        Af = squeeze(A(i_f,:,:));
+        
+        ApSigmaYinv  = Af' / (sigc(i_f) * eye(Nsensor) + Af * (repmat(gamma, [1 Nsensor] ) .* Af'));
+        
+        % Sum over snapshots and normalize, abs for roundoff errors
+        gamma_num(i_f,:)   = sum ( abs ( ( ApSigmaYinv * squeeze2(Y(i_f,:,:)) ).^2 ),2 ) / Nsnapshot;
+        
+        % positive def quantity, abs for roundoff errors
+        gamma_denum(i_f,:) = abs( sum  ( ApSigmaYinv.' .* Af, 1 ) );
+
+    end
+    
+    % Fixed point Eq. update
+    gamma = gamma  .* ((sum( gamma_num   ,1 ) ./...
+        sum( gamma_denum ,1 ) ).^(1/options.fixedpoint) ).' ;
+    
+    %% sigma and L2 error using unbiased posterior update 
+    
+    % locate same peaks for all frequencies
+    [ ~ , Ilocs] = findpeaks(gamma,'SORTSTR','descend','NPEAKS',Nsource);
+    Apeak      = A(:,:,Ilocs);
+    
+    for i_f = 1 : Nfreq
+        
+        % only active replicas
+        Am     = squeeze2(Apeak(i_f,:,:));
+   
+        % noise estimate
+        sigc(i_f) = real(trace( (eye(Nsensor)-Am*pinv(Am)) * squeeze(SCM(i_f,:,:)) ) / ( Nsensor- Nsource ) );
+
+    end
+    
+    %% Convergance
+    % checks convergance and displays status reports
+    
+    % convergance indicator
+    errornorm(j1) = norm ( gamma - gammaOld, 1 ) / norm ( gamma, 1 );
+    
+    % look into the past and find best error since then
+    if j1 > options.convergence.min_iteration  &&  errornorm(j1) < gmin_global
+        gmin_global = errornorm(j1);
+        gamma_min   = gamma;
+        iteration_L1   = j1;
+    end
+    
+    % inline convergence code
+    if j1 > options.convergence.min_iteration && ( errornorm(j1) < options.convergence.error  || iteration_L1 + options.convergence.delay <= j1)
+        
+        if options.flag == 1
+            display(['Solution converged. Iteration: ',num2str(sprintf('%.4u',j1)),'. Error: ',num2str(sprintf('%1.2e' , errornorm(j1) )),'.'])
+        end
+        break; % goodbye
+        
+        % not convereged
+    elseif j1 == options.convergence.maxiter
+        warning(['Solution not converged. Error: ',num2str(sprintf('%1.2e' , errornorm(j1) )),'.'])
+        
+        % status report
+    elseif j1 ~= options.convergence.maxiter  && options.flag == 1 && mod(j1,options.status_report) == 0 % Iteration reporting
+        display(['Iteration: ',num2str(sprintf('%.4u',j1)),'. Error: ',num2str(sprintf('%1.2e' , errornorm(j1) )),'.' ])
+        
+    end
+    
+end
+
+%% Posterior distribution
+% x_post - posterior unbiased mean
+for i_f = 1 : Nfreq
+    
+    Af = squeeze(A(i_f,:,:));
+    
+    x_post(i_f,:,:) = repmat(gamma, [1 Nsnapshot] ) .*...
+        (Af' / (sigc(i_f) * eye(Nsensor) +...
+         Af * (repmat(gamma, [1 Nsensor] ) .* Af')) * squeeze2(Y(i_f,:,:)));
+    
+end
+
+%% function return
+% Globla minimum
+gamma = gamma_min;
+
+% Report section
+
+% vectors containing errors
+report.results.error    = errornorm;
+
+% Error when minimum was obtained
+report.results.iteration_L1 = iteration_L1;
+
+% General info
+report.results.final_iteration.iteration = j1;
+report.results.final_iteration.noisepower = sigc;
+
+if options.tic == 1
+    report.results.toc = toc;
+else
+    report.results.toc = 0;
+end
+
+% data
+report.results.final_iteration.gamma  = gamma  ;
+report.results.final_iteration.x_post = x_post ;
+
+report.options = options;
+
+end
+
+function b = squeeze2(a)
+%   SQUEEZE2
+%   Just as squeeze but with transpose to accomodate single snapshot case.
+%   This is required because matlab does not allow a singleton dimension
+%   at the 'end', e.g., 3x5x1.
+%   The entire code might alternatively be re-written to have the number
+%   of sensors n~=1 at the end (making this fix obsolete).
+
+if ~ismatrix(a)
+    siz = size(a);
+    siz(siz==1) = []; % Remove singleton dimensions.
+    siz = [siz ones(1,2-length(siz))]; % Make sure siz is at least 2-D
+    b = reshape(a,siz);
+else
+    b = a.';
+end
+
+end
+
+%EOF
---
+++ example.m
@@ -0,0 +1,238 @@
+% Y. Park, F. Meyer, and P. Gerstoft
+% "Sequential sparse Bayesian learning for time-varying direction of arrival."
+% J. Acoust. Soc. Am. 149, 2089–2099 (2021).
+% https://doi.org/10.1121/10.0003802
+
+% Created by Yongsung Park, SIO UCSD
+% yongsungpark@ucsd.edu
+% Last updated 2021-03-29
+
+%%
+clear; clc;
+close all;
+
+% rngN = 0;
+% rng(rngN)
+
+dbstop if error;
+
+% addpath([cd,'/_common'])
+
+% SNRlist = [40 35 30 25 20 15 10 5 0];
+% SNRlist = [40 35 30 25 20 15 10 5];
+SNRlist = [20];
+for nSNR = 1:length(SNRlist)
+Nsim = 1;
+for nsim=1:Nsim
+Nrng=0; rng(Nrng+nsim)
+% rng(nsim)
+disp(['SNR',num2str(SNRlist(nSNR)),'_',num2str(nsim)])
+% Environment parameters
+c = 1500;       % speed of sound
+f = 200;        % frequency
+lambda = c/f;   % wavelength
+
+% ULA-horizontal array configuration
+Nsensor = 15;               % number of sensors
+d = 1/2*lambda;             % intersensor spacing
+q = (0:1:(Nsensor-1))';     % sensor numbering
+xq = (q-(Nsensor-1)/2)*d;   % sensor locations
+
+% sensor configuration structure
+Sensor_s.Nsensor = Nsensor;
+Sensor_s.lambda = lambda;
+Sensor_s.d = d;
+Sensor_s.q = q;
+Sensor_s.xq = xq;
+
+% signal generation parameters
+SNR = SNRlist(nSNR);
+
+% total number of snapshots
+Nsnapshot = 50;
+
+% range of angle space
+thetalim = [-90 90];
+
+theta_separation = 0.5;
+
+% Angular search grid
+theta = (thetalim(1):theta_separation:thetalim(2))';
+Ntheta = length(theta);
+
+% Design/steering matrix (Sensing matrix)
+sin_theta = sind(theta);
+sensingMatrix = exp(-1i*2*pi/lambda*xq*sin_theta.')/sqrt(Nsensor);
+
+% Generate received signal
+% anglesTrue = [-3; 2; 60]; % DOA of sources at first snapshot [deg]
+% source_amp = [  7;   7;  7;  4;  4; 13];
+anglesTrue = [-70; -55; -40; 35; 50; 65]; % DOA of sources at first snapshot [deg]
+anglesTracks = repmat(anglesTrue,[1,Nsnapshot]);
+anglesTracks(3,:) = anglesTracks(3,1) - 2*anglesTracks(3,1)./(1+exp(-.1*(-Nsnapshot/2:-Nsnapshot/2+Nsnapshot-1)));
+anglesTracks(4,:) = anglesTracks(4,1) - 1.00*(0:Nsnapshot-1)';
+sinAnglesTracks = sind(anglesTracks); 
+Nsources = numel(anglesTrue);
+
+receivedSignal = zeros(Nsensor,Nsnapshot);
+source_amp = zeros(Nsources,Nsnapshot);
+
+for snapshot = 1:Nsnapshot
+    ActSrcInd{snapshot} = [1;2;3;4;5;6];
+%     if snapshot <= 15
+%         ActSrcInd{snapshot} = [1;2;3;4];
+%     elseif snapshot <= 30
+%         ActSrcInd{snapshot} = [1;2;5;6];
+%     else
+%         ActSrcInd{snapshot} = [3;4;5;6];
+%     end
+end
+
+for snapshot = 1:Nsnapshot
+
+    srcind = ActSrcInd{snapshot};
+    
+    % Source amplitude
+%     source_amp(:,snapshot) = complex(randn(size(anglesTrue)),randn(size(anglesTrue)))/sqrt(2);
+%     Xsource = source_amp(srcind,snapshot);
+    source_amp = [  7;   7;  7;  4;  4; 13];
+    Xsource = source_amp.*exp(1i*2*pi*rand(Nsources,1));    % random phase
+    Xsource = Xsource(srcind);
+    
+    % Represenation matrix (steering matrix)
+    transmitMatrix = exp( -1i*2*pi/lambda*xq*sinAnglesTracks(srcind,snapshot).' )/sqrt(Nsensor);
+    
+    % Received signal without noise
+    receivedSignal(:,snapshot) = sum(transmitMatrix*diag(Xsource),2);
+    
+    % add noise to the signals
+    rnl = 10^(-SNR/20)*norm(Xsource);
+    nwhite = complex(randn(Nsensor,1),randn(Nsensor,1))/sqrt(2*Nsensor);
+    
+    e = nwhite * rnl;	% error vector
+    receivedSignal(:,snapshot) = receivedSignal(:,snapshot) + e;
+end
+
+%% Conventional beamforming (CBF)
+outputBeamformer = sensingMatrix' * receivedSignal;
+
+for snapshot = 1:Nsnapshot
+snapshot
+srcind = ActSrcInd{snapshot};
+%% Non-Sequential SBL
+    options = SBLSet();
+    options.convergence.error = 10^(-3);
+    options.Nsource = ceil(Nsensor/2);
+    
+    threshold = .01;
+    
+    [gamma, reportSBL] = SBL_v3p12( sensingMatrix, receivedSignal(:,snapshot), options );
+    [mu_val_SBL,peak_SBL] = findpeaks(gamma,'SORTSTR','descend','Npeaks', Nsensor);
+    theta_filter_SBL = (theta(peak_SBL(abs(mu_val_SBL)>threshold*max(abs(mu_val_SBL)))));
+    gamma_filter_SBL = abs( mu_val_SBL(abs(mu_val_SBL)>threshold*max(abs(mu_val_SBL))) );
+    
+    outputSBL = struct('gamma',gamma,'doas',theta_filter_SBL,'amps',gamma_filter_SBL);
+    
+    if nsim ==1 && snapshot ==1, outputsSBL = []; end
+    outputsSBL = [outputsSBL;outputSBL];
+
+%% Sequential SBL
+%     options = SBLSet();
+%     options.convergence.error = 10^(-3);
+%     options.Nsource = ceil(Nsensor/2);
+%     
+%     threshold = .01;
+    
+    if snapshot==1
+        [gammaSeq, reportSeqSBL] = SBL_v3p12( sensingMatrix, receivedSignal(:,snapshot), options );
+    else
+        [gammaSeq, reportSeqSBL] = SBLseq_v3p12( sensingMatrix, receivedSignal(:,snapshot), gammaSeq, options );
+    end
+    [mu_val_SBLseq,peak_SBLseq] = findpeaks(gammaSeq,'SORTSTR','descend','Npeaks', Nsensor);
+    theta_filter_SBLseq = (theta(peak_SBLseq(abs(mu_val_SBLseq)>threshold*max(abs(mu_val_SBLseq)))));
+    gamma_filter_SBLseq = abs( mu_val_SBLseq(abs(mu_val_SBLseq)>threshold*max(abs(mu_val_SBLseq))) );
+    
+    outputSBLseq = struct('gamma',gammaSeq,'doas',theta_filter_SBLseq,'amps',gamma_filter_SBLseq);
+    
+    if nsim ==1 && snapshot ==1, outputsSSBL = []; end
+    outputsSSBL = [outputsSSBL;outputSBLseq];
+
+end
+end
+% vars = who();
+% varnames = vars(contains(vars, 'outputs'));
+% save(['D_AD_CG6s_SNR',num2str(SNR),'_',num2str(nsim)],...
+%     'anglesTracks',varnames{:});
+end
+% save('data_results_AD')
+
+%% Plot: Non-sequential SBL
+outputPlot = outputsSBL;
+figure;
+set(gcf,'position',[562,250,560,420]);
+imagesc(1:Nsnapshot,theta,-inf);
+caxis([-20 0])
+
+rtIndex = []; rtTheta = []; rtAmp = [];
+for index=1:Nsnapshot
+    rTheta  = outputPlot(index).doas;
+    rAmp    = outputPlot(index).amps;
+    rAmp    = 10*log10( rAmp / max(rAmp) );
+    
+    rtIndex = [rtIndex;index*ones(size(rAmp))];
+    rtTheta = [rtTheta;rTheta];
+    rtAmp = [rtAmp;rAmp];
+end
+
+hold on; scatter(rtIndex,rtTheta,50,rtAmp,'filled','o','linewidth',.5,...
+    'MarkerEdgeColor','k'); hold off;
+
+title('Non-sequential SBL','interpreter','latex')
+xlabel('Time step','interpreter','latex')
+ylabel('DOA~[$^\circ$]','interpreter','latex')
+box on
+set(gca,'fontsize',18,'YDir','normal','TickLabelInterpreter','latex','YTick',-80:40:80)
+axis([.5 Nsnapshot+.5 -90 90])
+set(gca,'YTickLabel','')
+
+%% Plot: Sequential SBL
+outputPlot = outputsSSBL;
+figure;
+set(gcf,'position',[1124,250,560,420]);
+imagesc(1:Nsnapshot,theta,-inf);
+caxis([-20 0])
+
+rtIndex = []; rtTheta = []; rtAmp = [];
+for index=1:Nsnapshot
+    rTheta  = outputPlot(index).doas;
+    rAmp    = outputPlot(index).amps;
+    rAmp    = 10*log10( rAmp / max(rAmp) );
+    
+    rtIndex = [rtIndex;index*ones(size(rAmp))];
+    rtTheta = [rtTheta;rTheta];
+    rtAmp = [rtAmp;rAmp];
+end
+
+hold on; scatter(rtIndex,rtTheta,50,rtAmp,'filled','o','linewidth',.5,...
+    'MarkerEdgeColor','k'); hold off;
+
+title('Sequential SBL','interpreter','latex')
+xlabel('Time step','interpreter','latex')
+ylabel('DOA~[$^\circ$]','interpreter','latex')
+box on
+set(gca,'fontsize',18,'YDir','normal','TickLabelInterpreter','latex','YTick',-80:40:80)
+axis([.5 Nsnapshot+.5 -90 90])
+set(gca,'YTickLabel','')
+
+%% Conventional beamforming (CBF)
+figure;
+set(gcf,'position',[1,250,560,420]);
+imagesc(1:Nsnapshot,theta,10*log10( (abs(outputBeamformer).^2) ./ max((abs(outputBeamformer).^2),[],1)));
+caxis([-20 0])
+
+title('Conventional beamforming (CBF)','interpreter','latex')
+xlabel('Time step','interpreter','latex')
+ylabel('DOA~[$^\circ$]','interpreter','latex')
+box on
+set(gca,'fontsize',18,'YDir','normal','TickLabelInterpreter','latex','YTick',-80:40:80)
+axis([.5 Nsnapshot+.5 -90 90])
